e701cc13852d15649595267ee8476e2f12312270
Milan Carevic <96025559+carevicm@users.noreply.github.com>
2023-07-01 20:03:57 -0700

    Notifications.js

--- react_intro/task_5/dashboard/src/Notifications/Notifications.js
+++ react_intro/task_5/dashboard/src/Notifications/Notifications.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Notifications.css";
 import { getLatestNotification } from "../utils/utils";
 import closeIcon from "../assets/close-icon.png";
 
 const Notifications = () => {
+  const [showNotifications, setShowNotifications] = useState(true);
+
+  const handleCloseNotifications = () => {
+    setShowNotifications(false);
+  };
+
+  if (!showNotifications) {
+    return null;
+  }
+
   return (
     <div className="Notifications">
       <button
@@ -14,6 +24,7 @@ const Notifications = () => {
           right: 20,
         }}
         aria-label="close"
+        onClick={handleCloseNotifications}
       >
         <img src={closeIcon} alt="close-icon" />
       </button>
